57f99be713ec31a824eb449fcaa406e21d8008e7
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2022-04-12 17:34:31 -0400

    Deferring is not needed as pebble_ready will call this method anyway

--- src/charm.py
+++ src/charm.py
@@ -172,7 +172,7 @@ class GrafanaAgentOperatorCharm(CharmBase):
         if not self._container.can_connect():
             # Pebble is not ready yet so no need to update config
             self.unit.status = WaitingStatus("waiting for agent container to start")
-            event.defer()
+            return
             return
 
         config = self._config_file()
