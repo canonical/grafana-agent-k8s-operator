8d6a3fdd0c4d4f67ded5e3dd60c0b35e407f6e49
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2023-01-04 01:00:14 -0500

    Remove boilerplate docstring
    Co-authored-by: Leon <82407168+sed-i@users.noreply.github.com>

--- src/grafana_agent.py
+++ src/grafana_agent.py
@@ -1,26 +1,7 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""TODO: Add a proper docstring here.
-
-This is a placeholder docstring for this charm library. Docstrings are
-presented on Charmhub and updated whenever you push a new version of the
-library.
-
-Complete documentation about creating and documenting libraries can be found
-in the SDK docs at https://juju.is/docs/sdk/libraries.
-
-See `charmcraft publish-lib` and `charmcraft fetch-lib` for details of how to
-share and consume charm libraries. They serve to enhance collaboration
-between charmers. Use a charmer's libraries for classes that handle
-integration with their charm.
-
-Bear in mind that new revisions of the different major API versions (v0, v1,
-v2 etc) are maintained independently.  You can continue to update v0 and v1
-after you have pushed v3.
-
-Markdown is supported, following the CommonMark specification.
-"""
+"""Common logic for both k8s and machine charms for Grafana Agent."""
 import logging
 import os
 import pathlib
