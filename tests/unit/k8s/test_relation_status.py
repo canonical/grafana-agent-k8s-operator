fc682e5a8f4ce120ce26e4aff27699ec2034e8d9
sed-i <82407168+sed-i@users.noreply.github.com>
2023-03-20 17:23:27 -0400

    Add utests

---
+++ tests/unit/k8s/test_relation_status.py
@@ -0,0 +1,53 @@
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import unittest
+from unittest.mock import patch
+
+from ops.model import ActiveStatus, BlockedStatus
+from ops.testing import Harness
+
+from charm import GrafanaAgentK8sCharm as GrafanaAgentCharm
+
+
+@patch("charm.KubernetesServicePatch", lambda *_, **__: None)
+class TestRelationStatus(unittest.TestCase):
+    @patch("charm.KubernetesServicePatch", lambda *_, **__: None)
+    def setUp(self, *unused):
+        patcher = patch.object(GrafanaAgentCharm, "_agent_version", property(lambda *_: "0.0.0"))
+        self.mock_version = patcher.start()
+        self.addCleanup(patcher.stop)
+
+        self.harness = Harness(GrafanaAgentCharm)
+        self.harness.set_model_name(self.__class__.__name__)
+
+        self.addCleanup(self.harness.cleanup)
+        self.harness.set_leader(True)
+        self.harness.begin_with_initial_hooks()
+
+    def test_no_relations(self):
+        self.harness.charm.on.update_status.emit()
+        self.assertIsInstance(self.harness.charm.unit.status, ActiveStatus)
+
+    def test_with_relations(self):
+        for incoming, outgoing in [
+            # K8s
+            ("logging-provider", "logging-consumer"),
+            ("metrics-endpoint", "send-remote-write"),
+            ("grafana-dashboards-provider", "grafana-dashboards-consumer"),
+        ]:
+            with self.subTest(incoming=incoming, outgoing=outgoing):
+                # WHEN an incoming relation is added
+                rel_id = self.harness.add_relation(incoming, "grafana-agent")
+                self.harness.add_relation_unit(rel_id, "grafana-agent/0")
+                self.harness.update_relation_data(rel_id, "grafana-agent/0", {"dummy": "value"})
+
+                # THEN the charm goes into blocked status
+                self.assertIsInstance(self.harness.charm.unit.status, BlockedStatus)
+
+                # AND WHEN an appropriate outgoing relation is added
+                rel_id = self.harness.add_relation(outgoing, "grafana-agent")
+                self.harness.add_relation_unit(rel_id, "grafana-agent/0")
+
+                # THEN the charm goes into active status
+                self.assertIsInstance(self.harness.charm.unit.status, ActiveStatus)
