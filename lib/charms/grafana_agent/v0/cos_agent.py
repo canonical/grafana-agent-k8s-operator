d2c05037189983805d9add571bec4fe4550cdbcb
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2023-07-25 09:35:59 +0000

    pin pydantic

--- lib/charms/grafana_agent/v0/cos_agent.py
+++ lib/charms/grafana_agent/v0/cos_agent.py
@@ -238,7 +238,7 @@ LIBID = "dc15fa84cef84ce58155fb84f6c6213a"
 LIBAPI = 0
 LIBPATCH = 5
 
-PYDEPS = ["cosl", "pydantic<2"]
+PYDEPS = ["cosl", "pydantic < 2"]
 
 DEFAULT_RELATION_NAME = "cos-agent"
 DEFAULT_PEER_RELATION_NAME = "peers"
