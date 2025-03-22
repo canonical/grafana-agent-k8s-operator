6caae3081f396394079d38e67c61b3afef386d49
sed-i <82407168+sed-i@users.noreply.github.com>
2025-03-22 00:32:40 -0400

    Consolidate into charmcraft.yaml

--- tests/integration/test_upgrade_charm.py
+++ tests/integration/test_upgrade_charm.py
@@ -11,7 +11,7 @@ import yaml
 
 logger = logging.getLogger(__name__)
 
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 app_name = METADATA["name"]
 
 
