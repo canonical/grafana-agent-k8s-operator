6d7f0918eff964f785c3248c634e62e2a3fd379c
Luca Bello <luca.bello@canonical.com>
2025-06-18 11:20:03 +0200

    address pr comments

--- tests/unit/test_endpoint_deduplication.py
+++ tests/unit/test_endpoint_deduplication.py
@@ -35,23 +35,22 @@ def test_prometheus_endpoints_deduplication():
     # WHEN the charm processes endpoints with duplicates
     ctx = Context(charm_type=GrafanaAgentK8sCharm)
 
-    with ctx(ctx.on.config_changed(), state) as mgr:
-        output_state = mgr.run()
-        agent = output_state.get_container("agent")
+    output_state = ctx.run(ctx.on.config_changed(), state)
+    agent = output_state.get_container("agent")
 
-        # THEN the agent has started
-        assert agent.services["agent"].is_running()
-        # AND the grafana agent config has deduplicated the endpoints
-        fs = agent.get_filesystem(ctx)
-        gagent_config = fs.joinpath(*CONFIG_PATH.strip("/").split("/"))
-        assert gagent_config.exists()
-        yml = yaml.safe_load(gagent_config.read_text())
-        # check endpoints are deduplicated in the metrics section
-        metrics_config = yml["metrics"]["configs"][0]
-        assert metrics_config["name"] == "agent_scraper"
-        assert len(metrics_config["remote_write"]) == 1
-        assert metrics_config["remote_write"][0]["url"] == remote_write_url
-        # check endpoints are deduplicated in the integrations section
-        integrations_config = yml["integrations"]
-        assert len(integrations_config["prometheus_remote_write"]) == 1
-        assert integrations_config["prometheus_remote_write"][0]["url"] == remote_write_url
+    # THEN the agent has started
+    assert agent.services["agent"].is_running()
+    # AND the grafana agent config has deduplicated the endpoints
+    fs = agent.get_filesystem(ctx)
+    gagent_config = fs.joinpath(*CONFIG_PATH.strip("/").split("/"))
+    assert gagent_config.exists()
+    yml = yaml.safe_load(gagent_config.read_text())
+    # check endpoints are deduplicated in the metrics section
+    metrics_config = yml["metrics"]["configs"][0]
+    assert metrics_config["name"] == "agent_scraper"
+    assert len(metrics_config["remote_write"]) == 1
+    assert metrics_config["remote_write"][0]["url"] == remote_write_url
+    # check endpoints are deduplicated in the integrations section
+    integrations_config = yml["integrations"]
+    assert len(integrations_config["prometheus_remote_write"]) == 1
+    assert integrations_config["prometheus_remote_write"][0]["url"] == remote_write_url
