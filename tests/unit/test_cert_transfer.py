6fc5954f045980af64b930419bfb3525aaab5e8c
Sina P <55766091+sinapah@users.noreply.github.com>
2025-10-06 16:21:08 -0400

    fix: delete CA cert on receive-ca-cert relation removed (#412)
    * fix: delete CA cert on receive-ca-cert relation removed
    
    * fix: remove log
    
    * chore: add test
    
    * fix: remove unrelated test
    
    * chore: add test
    
    * fix: lint
    
    * fix: inclusive name check
    
    * chore: remove cert write test
    
    * chore: fetch lib
    
    * chore: bump cosl
    
    * fix: update comment
    
    * chore: fetch lib

--- tests/unit/test_cert_transfer.py
+++ tests/unit/test_cert_transfer.py
@@ -1,12 +1,15 @@
 
+
 import json
+import pathlib
+import tempfile
 import uuid
 from pathlib import Path
 
 import yaml
 from helpers import k8s_resource_multipatch, patch_lightkube_client
 from ops import pebble
-from ops.testing import Container, Exec, Model, Relation, State
+from ops.testing import Container, Exec, Model, Mount, Relation, State
 
 
 @patch_lightkube_client
@@ -67,3 +70,56 @@ def test_ca_cert_saved_to_disk(ctx):
     # AND the content of the cert file is equal to the certificate in relation data
     cert = yaml.safe_load(Path(cert_path).read_text())
     assert cert == fake_cert
+
+@patch_lightkube_client
+@k8s_resource_multipatch
+def test_ca_cert_removed_from_disk_on_relation_broken(ctx):
+    model_uuid = uuid.uuid4()
+    ca_cert_path = "/usr/local/share/ca-certificates"
+    fake_cert = "-----BEGIN CERTIFICATE-----123-----END CERTIFICATE-----"
+    remote_app_data = {
+        "certificates": json.dumps([
+            fake_cert
+        ]),
+        "version": "1"
+    }
+    # GIVEN a receive-ca-cert relation over certificates_transfer
+    rel_id = 1
+    certificate_transfer_relation = Relation(
+        "receive-ca-cert",
+        remote_app_data=remote_app_data,
+        id=rel_id,
+    )
+
+    # Creating a fake CA cert file
+    # This file will need to be mounted into the container. When the relation is broken, we'll check that the file has been deleted.
+
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        fake_path = pathlib.Path(tmp.name)
+    cert_path = f"{ca_cert_path}/receive-ca-cert-{model_uuid}-{rel_id}-0-ca.crt"
+    mounts = {
+        'fake-ca-cert': Mount(location=cert_path, source=fake_path),
+    }
+
+    state = State(
+        leader=True,
+        containers=[
+            Container(
+                "agent",
+                can_connect=True,
+                execs=[Exec(['update-ca-certificates', '--fresh'])],
+                mounts=mounts,
+            ),
+        ],
+        relations=[certificate_transfer_relation],
+        model=Model(uuid=str(model_uuid)),
+    )
+    # WHEN a relation is broken
+    state_out = ctx.run(ctx.on.relation_broken(certificate_transfer_relation), state)
+
+    # THEN the fake CA cert file should be deleted
+    agent = state_out.get_container("agent")
+
+    fs = agent.get_filesystem(ctx)
+    ca_cert_path = fs.joinpath(*ca_cert_path.strip("/").split("/"))
+    assert list(ca_cert_path.iterdir()) == []
