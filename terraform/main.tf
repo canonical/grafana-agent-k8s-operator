fac7ad4ca223664a0f0d24574f157de077baf4ed
Michael Thamm <mike.thamm@canonical.com>
2025-10-29 10:39:57 -0400

    feat: TF Juju provider v1

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "juju_application" "grafana_agent" {
   name               = var.app_name
   config             = var.config
   constraints        = var.constraints
-  model              = var.model
+  model_uuid         = var.model_uuid
   storage_directives = var.storage_directives
   trust              = true
   units              = var.units
